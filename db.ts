128ecd446a707f4c29304b72728e77ccb9639861
MD. Alamin <vijayalamin@gmail.com>
2023-06-03 22:45:45 +0600

    husky n linters

--- db.ts
+++ db.ts
@@ -1,13 +1,14 @@
-import { error, log } from 'console';
-import mongoose from 'mongoose';
+import { error, log } from 'console'
+import mongoose from 'mongoose'
 
 const connectToDB = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.pfaf1en.mongodb.net/zeron?retryWrites=true&w=majority`);
-        log("connect to database")
-    } catch (err) {
-        error(err)
-    }
-
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.pfaf1en.mongodb.net/zeron?retryWrites=true&w=majority`
+    )
+    log('connect to database')
+  } catch (err) {
+    error(err)
+  }
 }
 export default connectToDB
