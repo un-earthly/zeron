23c1ecce5b8d6a279f1eec0034a3bd109a128ae9
MD. Alamin <vijayalamin@gmail.com>
2023-06-09 19:52:36 +0600

    logger

--- db.ts
+++ db.ts
@@ -1,14 +1,15 @@
 import { error, log } from 'console'
 import mongoose from 'mongoose'
+import { errorlogger, successLogger } from './app/shared/logger'
 
 const connectToDB = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.pfaf1en.mongodb.net/zeron?retryWrites=true&w=majority`
     )
-    log('connect to database')
+    successLogger.info(`🛢   Database is connected successfully`)
   } catch (err) {
-    error(err)
+    errorlogger.error('Failed to connect database', err)
   }
 }
 export default connectToDB
